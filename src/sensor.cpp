8e4c804c8c57f756e650ba74478dc4ec9c1510e6
Joe <jmaurer1994@gmail.com>
2023-12-22 13:27:21 -0500

    improve data capturing

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -9,7 +9,7 @@ int samples_collected = 0;
 volatile bool sensor_activity = false;
 volatile bool sampling_active = false;
 
-std::vector<ForceEvent> forceEvents;
+std::vector<ForceEvent> events;
 
 void IRAM_ATTR SAMPLE_START_ISR() {
     if (!sampling_active) {
@@ -17,7 +17,24 @@ void IRAM_ATTR SAMPLE_START_ISR() {
     }
 }
 
+class ForceEvent{
+    uint64_t timestamp;
+    std::vector<float> samples;
+public:
+    void record_sample(float  value){
+        samples.push_back(value);
+    }
+    void init_object(){
+        timestamp = micros();
+        samples.clear();
+    }
 
+    int samples_collected(){
+        return samples.size();
+    }
+};
+
+ForceEvent *current_event; //holds a pointer to the current sample
 
 bool sample_force_sensor() {
     if (!ADS.isConnected()) {
@@ -32,31 +49,25 @@ bool sample_force_sensor() {
     if (sensor_activity && !sampling_active) {
         DEBUG_PRINTLN("Started sampling");
         sampling_active = true;
+        current_event->init_object();
     }
 
     // do sampling
     float force = ADS.getValue();
+
     if (force < 100) {
         // stop sampling
-        if(samples_collected > 0){
-        DEBUG_PRINTF3("Event recorded:\n\tAvg: %f\tpeak: %f\tsamples: %d\n",
-                      total_force / samples_collected, peak_force,
-                      samples_collected);
-        }
-        total_force = 0;
-        peak_force = 0;
-        samples_collected = 0;
+        if(current_event->samples_collected() > 0){
+            events.push_back(current_event);
+            current_event = new ForceEvent; //allocate another object
+        } // don't save phantom events (happens on wifi dc?)
+
         sensor_activity = false; // reset int
         sampling_active = false;
         return false;
     }
 
-    if (force > peak_force) {
-        peak_force = force;
-    }
-
-    total_force += force;
-    samples_collected++;
+    current_event->record_sample(force);
 
     return true;
 }
@@ -81,6 +92,8 @@ bool initialize_force_sensor() {
 
     ADS.requestADC(0);
     DEBUG_PRINTLN("Intialized ADC"); // Sends above settings to ADC
+
+    current_event = new ForceEvent;
 
     pinMode(EADC_ALERT_PIN, INPUT_PULLUP);
     DEBUG_PRINTLN("Configured alert pin");
